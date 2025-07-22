46c4ada792262b5a5bf9e77c5ab463bf5ef68da3
Blackhawk364 <2236001477@utna.edu.mx>
2025-07-22 17:52:24 -0600

    Se agrego el CRUD

--- app/controllers/ejercicioController.js
+++ app/controllers/ejercicioController.js
@@ -1,25 +1,138 @@
+const EjerciciosModel = require("../models/EjerciciosModel");
 
-const ejercicioModel = require('../models/EjerciciosModel'); // Ruta a tu modelo
+function BuscarTodo(req, res) {
+  EjerciciosModel.find({})
+    .then((ejercicios) => {
+      if (ejercicios.length) {
+        return res.status(200).send({ ejercicios });
+      }
+      return res.status(204).send({
+        message: "no hay info",
+      });
+    })
+    .catch((err) => {
+      return res.status(404).send({
+        message: `no jala la consulta: ${err}`,
+      });
+    });
+}
 
-function buscarTodo(req, res) {
-    ejercicioModel.find({})
-        .then(ejercicios => {
+function GuardarEjercicio(req, res) {
+  console.log(req.body);
+  new EjerciciosModel(req.body)
+    .save()
+    .then((info) => {
+      return res.status(200).send({
+        message: "info guardada",
+        info,
+      });
+    })
+    .catch((err) => {
+      return res.status(404).send({
+        message: `info no guardada: ${err}`,
+      });
+    });
+}
 
-            if (ejercicios.length > 0) {
-                return res.status(200).send({ ejercicios });
-            } else {
+function BuscarEjercicio(req, res, next) {
+  const consulta = {};
+  consulta[req.params.key] = req.params.valor;
+  EjerciciosModel.findOne(consulta) // Changed from find to findOne
+    .then((info) => {
+      if (!info) {
+        // If no document found, info will be null
+        return next();
+      }
+      req.body = req.body || {};
+      req.body.ejercicios = info; // info is now a single document
+      return next();
+    })
+    .catch((err) => {
+      req.body = req.body || {};
+      req.body.err = err;
+      next();
+    });
+}
 
-                return res.status(204).send();
+function mostrarEjercicio(req, res) {
+  if (req.body.err)
+    return res.status(404).send({
+      message: `no jala la busqueda`,
+      error: req.body.err,
+    });
+  if (!req.body.ejercicios)
+    return res.status(204).send({
+      message: "no hay info",
+    });
+  const ejercicio = req.body.ejercicios;
+  return res.status(200).send({ ejercicio });
+}
 
-            }
-        })
-        .catch(error => {
-   
-            console.error("Error al buscar ejercicios:", error); // **MUY IMPORTANTE: revisa tu consola del servidor**
-            return res.status(500).send({ mensaje: `ERROR DE CONSULTA: ${error.message || error}` });
-        });
+function eliminarEjercicio(req, res) {
+  if (req.body.err)
+    return res.status(404).send({
+      message: `no jala la busqueda`,
+      error: req.body.err,
+    });
+  if (!req.body.ejercicios)
+    return res.status(204).send({
+      message: "no hay info",
+    });
+
+  req.body.ejercicios
+    .deleteOne()
+    .then((info) => {
+      return res.status(200).send({
+        message: "ejercicio eliminado",
+        info,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: `ejercicio no eliminado`,
+        err,
+      });
+    });
+}
+
+function actualizarEjercicio(req, res) {
+  if (req.body.err) {
+    return res.status(404).send({
+      message: `no se encontró el ejercicio para actualizar`,
+      error: req.body.err,
+    });
+  }
+  if (!req.body.ejercicios) {
+    return res.status(204).send({
+      message: "no hay info para actualizar",
+    });
+  }
+
+  // Los datos a actualizar se espera que vengan en req.body
+  // Se asume que req.body.ejercicios ya contiene el ejercicio encontrado por BuscarEjercicio
+  Object.assign(req.body.ejercicios, req.body);
+
+  req.body.ejercicios
+    .save()
+    .then((info) => {
+      return res.status(200).send({
+        message: "ejercicio actualizado",
+        info,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: `no se pudo actualizar el ejercicio`,
+        err,
+      });
+    });
 }
 
 module.exports = {
-    buscarTodo
+  BuscarTodo,
+  GuardarEjercicio,
+  BuscarEjercicio,
+  mostrarEjercicio,
+  eliminarEjercicio,
+  actualizarEjercicio
 };
--- app/routes/gymRouter.js
+++ app/routes/gymRouter.js
@@ -1,8 +1,14 @@
-const express = require ('express')
-const router = express.Router()
-const ejercicioController = require('../controllers/ejercicioController')
+const express = require('express');
+const router = express.Router();
+const EjerciciosController = require('../controllers/ejercicioController');
 
-router.get('/ejercicios', ejercicioController.buscarTodo)
+router.get('/ejercicios', EjerciciosController.BuscarTodo)
+.post('/ejercicios', EjerciciosController.GuardarEjercicio)
 
-module.exports=router 
+router.get('/ejercicios/:key/:valor', EjerciciosController.BuscarEjercicio, EjerciciosController.mostrarEjercicio)
 
+router.delete('/ejercicios/:key/:valor', EjerciciosController.BuscarEjercicio, EjerciciosController.eliminarEjercicio)
+
+router.put('/ejercicios/:key/:valor', EjerciciosController.BuscarEjercicio, EjerciciosController.actualizarEjercicio);
+
+module.exports = router
